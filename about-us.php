8e8df885b980b7797c2cd9cae5392e1ff19d75ae
rokhsanakhushi <81320126+rokhsanakhushi@users.noreply.github.com>
2021-06-05 09:29:26 +0600

    About us page redesign

---
+++ about-us.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Recycle Bin</title>
+    <link rel="stylesheet" href="./semantic-css/semantic.min.css">
+</head>
+
+<body>
+
+    <div class="ui grid">
+        <div class="ui three wide column" style="background-image: url('./images/back.jpg'); background-repeat: no-repeat;
+                    background-attachment: fixed;
+                    background-size: cover; min-height: 100vh;">
+            <div style="padding: 20px">
+                <a href="home.php"><img style="width:200px;margin-left:auto;margin-right:auto;display:block " src="./images/logo.png"> </a>
+                
+                <h1 style="text-align:center;">Recycle Bin</h1>
+                <a href="../About-Us-Page/about-us.php">
+                    <h2 style="text-align:center;color:black;padding-top: 20px;">About Us</h2>
+                </a>
+            </div>
+            <div style="padding:30px">
+            </div>
+        </div>
+        <div class="ui thirteen wide column" style="background-image: url('./images/yes.png'); background-repeat: no-repeat ;background-size: cover; min-height: 100vh;">
+            <h1 style="color: black;text-align: center; font-size: 7vh;padding-top: 30px;font-weight: 900;">!! About Us !!</h1>
+            <p style="font-size: 26px;margin: 30px;font-weight: 600">Recycle Bin is a place where people will be able to advertise and sell their used products . We are providing this service from 2021 around Bangladesh.<br><br> Fully free of cost, because our moto is the holy human welfare and stopping Environment pollution. .<br><br>
+People can be benefited by using this site with no cost, avoiding physical movement and avoiding any further cost for move ment. </p>
+
+        <br>
+    </div>
+    </div>
+
+
+    <br><br>
+
+
+    <!-- Rest of the page content -->
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/semantic.min.js"></script>
+</body>
+
+</html>
