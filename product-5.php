dd7a399c02364077e60d776db91fd62573c32748
rokhsanakhushi <81320126+rokhsanakhushi@users.noreply.github.com>
2021-06-05 09:37:40 +0600

    Different Product page (category wise)

---
+++ product-5.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Search</title>
+    <link rel="stylesheet" href="./semantic-css/semantic.min.css">
+</head>
+
+<body>
+    <div class="ui fluid menu">
+       
+        <div style="margin-left: 20px" class="header item">
+            Recycle Bin
+        </div>
+      <div class="right menu">
+        <a class="item active" href="../HomePage/homepage.php">Home</a>
+        <a  href="../About-Us-Page/about-us.php" class="item">About US</a>
+        <a class="item">Seller Dashboard</a>
+        <a class="item">Sign Up</a>
+        <a class="item">Sign In</a>
+        <a class="item">Log Out</a>
+      </div>
+    </div>
+ 
+    <div class="ui grid">
+        <div class="ui twelve wide column" style="background-image: url('./images/back.jpg'); background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover; min-height: 100vh;">
+            <div style="padding: 20px">
+                <img style="height: 400px;width: 1000px; margin-left:auto;margin-right:auto;display:block " src="./images/product5.jpg">
+            </div>
+            <h1 style="padding: 10px; text-align: center;" class="center">Product Details:</h1>
+            <p style="text-align: center; padding: 10px 50px 100px 50px">The glass back is a huge fingerprint magnet, and, again, just like the other two new iPhone models, the iPhone X is prone to sliding off surfaces that are even slightly slippery or have even a bit of an incline. Be sure to protect your ‘precious’ in some sort of a case, or you could end up with a repair bill that rivals the GDP of a small nation. The dual rear cameras on the iPhone X are laid out vertically, as opposed to horizontally on the Plus-sized models. We found the mute button on the iPhone X to be stiffer than expected, requiring more force than usual to toggle on two different units that we tested.</p>
+
+
+             <div style="padding:0 10% 0 10%" class="ui form">
+                  <div class="field">
+                    <labels>Comment:</label>
+                    <textarea></textarea>
+                  </div>
+                  <button style="margin-top: 10px" class="ui center floated fluid button" type="submit">Comment</button>
+                </div>
+            <div style="padding:30px">
+            </div>
+        </div>
+        <div class="ui four wide column" style="background-image: url('./images/back2.jpg'); background-repeat: no-repeat ;background-size: cover; min-height: 100vh;">
+          <div style="padding: 20px" class="ui card">
+            <div class="image">
+              <img src="./images/men.jpg">
+            </div>
+            <div class="content">
+              <a class="header">Mehedi Hassan</a>
+              <div class="meta">
+                <span class="date">Owner</span>
+              </div>
+              <div class="description">
+                He is an the owner of this shop . Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.
+              </div>
+              <p>Dhaka , Bangladesh</p>
+
+            </div>
+            <div class="extra content">
+              <div class="ui star rating" data-rating="5"></div>
+            </div>
+          </div>
+
+        </div>
+    </div>
+
+
+
+    <!-- Rest of the page content -->
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/semantic.min.js"></script>
+</body>
+
+</html>
